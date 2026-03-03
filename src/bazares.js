f636aafa06338ada691bc41e192fa9e5cc2bf76a
gabrieladutra <gabriela_ddutra@outlook.com>
2026-03-02 21:19:39 -0300

    Adicionando assets

---
+++ src/bazares.js
@@ -0,0 +1,40 @@
+import abibe from "./assets/abibe.jpg"
+import perpetuo from "./assets/perpetuo.jpg"
+import divino from "./assets/divino.jpeg"
+import caritas from "./assets/caritas.png"
+import acridas from "./assets/acridas.jpg"
+
+export const bazares = [
+  {
+    id: 1,
+    titulo: "Bazar Abibe Isfer",
+    descricao: "Alameda Cabral, 275 - Centro, Curitiba - PR",
+    img: abibe
+  },
+  {
+    id: 2,
+    titulo: "Bazar Perpétuo Socorro",
+    descricao: "Rua Ivo Leão, 220",
+    img: perpetuo
+  },
+  {
+    id: 3,
+    titulo: "Bazar Divino Espírito Santo",
+    descricao: "R. Mateus Leme, 1855 - Bom Retiro, Curitiba - PR",
+    img: divino
+  },
+  {
+    id: 4,
+    titulo: "Bazar da Cáritas",
+    descricao: "Av. Mal. Floriano Peixoto, 8520 - Boqueirão, Curitiba - PR, 81670-000",
+    img: caritas
+  },
+   {
+    id: 5,
+    titulo: "Bazar Solidário Acridas",
+    descricao: "R. Eduardo Geronasso, 1782 Bacacheri, Curitiba – PR82510-280",
+    img: acridas
+  }
+
+
+]
