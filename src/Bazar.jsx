648b4956111ece3b5e93f2ab0d0cee6f9e3aa11c
gabrieladutra <gabriela_ddutra@outlook.com>
2026-03-09 11:54:26 -0300

    Adicionado carrossel de imagens

--- src/Bazar.jsx
+++ src/Bazar.jsx
@@ -5,17 +5,42 @@ import 'react-multi-carousel/lib/styles.css';
 import { useState } from "react";
 import { galeria } from "./galeria";
 
-export function Informacoes({ bazar, img }) {
+export function Informacoes({ bazar, img, fotos }) {
+    const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 2
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+}
     return (
         <div className="text-2xl flex flex-col items-center gap-8">
             <img src={img} alt="" className="h-[250px] w-[250px] object-cover" />
-            <h2 >{bazar.titulo}</h2>
+            <h2>{bazar.titulo}</h2>
             <ul className="text-xl">
                 <li>Endereço: {bazar.endereco}</li>
                 <li>Horários: {bazar.horarios}</li>
-                <li>Contatos {bazar.contatos}</li>
+                <li>Contatos: {bazar.contatos}</li>
             </ul>
-            <div>Galeria de Fotos</div>
+            <div>
+                <h2>Galeria de Fotos</h2>
+                <Carousel responsive={responsive} swipeable={false} draggable={false} transitionDuration={5000}>
+                    {fotos.map((foto, index) => (
+                        <img
+                            key={index}
+                            src={foto.img}
+                            className="h-[200px] w-[200px] object-cover"
+                        />
+                    ))}
+                </Carousel>
+            </div>
         </div>
     )
 }
@@ -30,16 +55,16 @@ export default function Bazar() {
         contatos: "11977317756",
     }
     return (
-        
-        <div className="min-h-screen pt-5 pl-5 border border-red-400 w-1/2 flexjustify-center">
-             <button onClick={() => navigate("/bazares/")}
-                    className="bg-red-400 text-white h-11 px-4 flex items-center gap-2 cursor-pointer md:w-60 md:justify-center md:text-lg">
-                    Voltar
-                </button>
+
+        <div className="min-h-screen pt-5 pl-5 border border-red-400 w-1/2 flex justify-center">
+            <button onClick={() => navigate("/bazares/")}
+                className="bg-red-400 text-white h-11 px-4 flex items-center gap-2 cursor-pointer md:w-60 md:justify-center md:text-lg">
+                Voltar
+            </button>
             <div>
-                <Informacoes bazar={bazar} img={abasc}></Informacoes>
+                <Informacoes bazar={bazar} img={abasc} fotos={listaFotos}></Informacoes>
             </div>
-            
+
         </div>
 
     )
