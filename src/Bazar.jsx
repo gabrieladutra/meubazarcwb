d9b9741e5d7043550b4e2d9197d6f412e9b1c501
gabrieladutra <gabriela_ddutra@outlook.com>
2026-03-09 11:22:59 -0300

    WIP

--- src/Bazar.jsx
+++ src/Bazar.jsx
@@ -1,12 +1,40 @@
+import abasc from "./assets/abasc.png"
+import { useNavigate } from "react-router-dom"
 
-
-export default function Bazar() {
+export function Informacoes({ bazar, img }) {
     return (
-        <div className="min-h-screen pt-5 pl-5 border border-red-400 w-1/2">
-            <div >Hello World</div>
-            <div></div>
-            <div></div>
+        <div className="text-2xl flex flex-col items-center gap-8">
+            <img src={img} alt="" className="h-[250px] w-[250px] object-cover" />
+            <h2 >{bazar.titulo}</h2>
+            <ul className="text-xl">
+                <li>Endereço: {bazar.endereco}</li>
+                <li>Horários: {bazar.horarios}</li>
+                <li>Contatos {bazar.contatos}</li>
+            </ul>
+            <div>Galeria de Fotos</div>
         </div>
+    )
+}
+export default function Bazar() {
+    const navigate = useNavigate()
+    const bazar = {
+        titulo: "Bazar da Abasc",
+        endereco: "Alameda Cabral 47",
+        horarios: "Seg a sex das 9 s 17",
+        contatos: "11977317756",
+    }
+    return (
         
+        <div className="min-h-screen pt-5 pl-5 border border-red-400 w-1/2 flexjustify-center">
+             <button onClick={() => navigate("/bazares/")}
+                    className="bg-red-400 text-white h-11 px-4 flex items-center gap-2 cursor-pointer md:w-60 md:justify-center md:text-lg">
+                    Voltar
+                </button>
+            <div>
+                <Informacoes bazar={bazar} img={abasc}></Informacoes>
+            </div>
+            
+        </div>
+
     )
 }
