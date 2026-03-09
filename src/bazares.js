17d1df94924c3fc9f7512d909b84549f8ba5ae1c
gabrieladutra <gabriela_ddutra@outlook.com>
2026-03-09 08:23:12 -0300

    Adicionando assets

--- src/bazares.js
+++ src/bazares.js
@@ -1,8 +1,17 @@
 import abibe from "./assets/abibe.jpg"
-import perpetuo from "./assets/perpetuo.jpg"
-import divino from "./assets/divino.jpeg"
-import caritas from "./assets/caritas.png"
+import abasc from "./assets/abasc.png"
 import acridas from "./assets/acridas.jpg"
+import apae from "./assets/apae.jpg"
+import cacamba from "./assets/cacamba.jpg"
+import caritas from "./assets/caritas.png"
+import ceefa from "./assets/ceefa.jpg"
+import cotolengo from "./assets/cotolengo.png"
+import divino from "./assets/divino.jpeg"
+import perpetuo from "./assets/perpetuo.jpg"
+
+
+
+
 
 export const bazares = [
   {
@@ -34,7 +43,73 @@ export const bazares = [
     titulo: "Bazar Solidário Acridas",
     descricao: "R. Eduardo Geronasso, 1782 Bacacheri, Curitiba – PR82510-280",
     img: acridas
+  },
+
+  {
+    id: 6,
+    titulo: "Bazar Santuário Nosa Sra. de Guardalupe",
+    descricao: "Praça Senador Correia, 128 - Curitiba PR",
+    img: acridas
+  },
+  {
+    id: 7,
+    titulo: "Bazar do Santíssimo",
+    descricao: "Praça Senador Correia, 128 - Curitiba PR",
+    img: acridas
+  },
+  {
+    id: 8,
+    titulo: "Caçamba do Bem",
+    descricao: "Av. Manoel Ribas, 2658",
+    img: cacamba
+  },
+  {
+    id: 9,
+    titulo: "Bazar ABASC/PIB",
+    descricao: " Rua Bento Viana, 1200 - Batel-Curitiba PR",
+    img: abasc
+  },
+  {
+    id: 10,
+    titulo: "Bazar APAE Curitiba",
+    descricao: "Avenida Nossa Senhora da Paz, 1041 - Boqueirão",
+    img: apae
+  },
+  {
+    id: 11,
+    titulo: "Bazar Missão Gênesis",
+    descricao: "Rua São Francisco, 126 – Centro",
+    img: acridas
+  },
+  {
+    id: 12,
+    titulo: "Bazar da Amizade - Pequeno Cotolengo",
+    descricao: "Rua São Francisco, 126 – Centro",
+    img: cotolengo
+  },
+  {
+    id: 13,
+    titulo: "Bazar Amigos do HC",
+    descricao: "Rua São Francisco, 126 – Centro",
+    img: acridas
+  },
+  {
+    id: 14,
+    titulo: "Bazar CEEFA",
+    descricao: "Rua Pref. Ângelo Ferrário Lopes , 1260 Curitiba - PR",
+    img: ceefa
+  },
+  {
+    id: 15,
+    titulo: "Bazar da Pró-Renal",
+    descricao: "Rua Pref. Ângelo Ferrário Lopes , 1260 Curitiba - PR",
+    img: acridas
   }
+
+
+
+
+
 
 
 ]
