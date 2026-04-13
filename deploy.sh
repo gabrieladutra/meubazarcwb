e26d7efa77adcc1cde8378f1e9ab86c1b8e71d5a
gabrieladutra <gabriela_ddutra@outlook.com>
2026-04-13 09:55:27 -0300

    Adicioando deploy da versão 1

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-zip -r -q puppeteer.zip bazares.js  node_modules/ package.json
-rm puppeteer.zip
+# Nome do bucket
+BUCKET='meubazarcwb.gabrieladutra.com'
+
+# 1) GERAR O BUILD
+echo "Gerando build..."
+npm run build
+
+# 2) LIMPAR BUCKET
+echo "Limpando bucket..."
+aws s3 rm s3://$BUCKET --recursive
+
+# 3) ENVIAR NOVO BUILD
+echo "Enviando novos arquivos..."
+aws s3 cp --acl public-read dist s3://$BUCKET --recursive
+
+# 4) INVALIDAR CLOUDFRONT
+echo "Invalidando CloudFront..."
+aws cloudfront create-invalidation \
+  --distribution-id E2KKZWTR0BTD3K \
+  --paths '/*' \
+  --no-cli-pager
+
+echo "Deploy finalizado com sucesso!"
